9cb152c3e98eb6890532cba54dadea6f5c241607
Josh <jdanielson04@gmail.com>
2020-08-08 13:27:37 -0700

    setup groundwork

---
+++ src/projects.js
@@ -0,0 +1,8 @@
+var projectList = [];
+
+function generateProject(title, priority, dueDate, discription) {
+    this.title = title;
+    this.priority = priority;
+    this.dueDate = dueDate;
+    this.discription = discription;
+}
